66b01b2d4336c1d2a604e403ffcff97b18fa12f0
huhn <sehe89@gmail.com>
2019-04-07 18:31:54 +0200

    add some sections and content

---
+++ src/components/Home.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="home">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Projektmanagement - Baumanagement</h1>
+          <h2
+            class="subtitle"
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</h2>
+          <img class="hero-image" src="@/assets/hero.jpg" alt>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Projektmanagement</h1>
+        <div class="columns">
+          <div class="column is-three-fifths">
+            <h2
+              class="subtitle"
+            >Von der Idee zur Wirklichkeit. Wir steuern die Konzeption, Erstellung und Budgetplanung von Immobilienprojekten und begleiten das Projekt vom Grundstückserwerb über die Planung, die Finanzierung bis hin zur Realisierung.</h2>
+          </div>
+
+          <div class="column">
+            <img src="@/assets/projektmanagement.jpg" alt>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <h1 class="title">Baumanagement</h1>
+        <div class="columns">
+          <div class="column is-three-fifths">
+            <h2
+              class="subtitle"
+            >Unser Ziel ist es, ein Bauprojekt so zu planen und abzuwickeln, dass die vereinbarten Termine und Kosten eingehalten werden und die Qualität des Bauwerks eine optimale Nutzung gewährleistet, und zwar unter vollständiger Berücksichtigung der ästhetischen, energetischen und ökologischen Anforderungen des Bauherrn und des Gesetzgebers.</h2>
+          </div>
+
+          <div class="column">
+            <img src="@/assets/baumanagement.jpg" alt>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="columns">
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img
+                  src="https://bulma.io/images/placeholders/1280x960.png"
+                  alt="Placeholder image"
+                >
+              </figure>
+            </div>
+            <div class="card-content">
+              <p class="title">Lorem ipsum</p>
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>.
+                <a href="#">#css</a>
+                <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img
+                  src="https://bulma.io/images/placeholders/1280x960.png"
+                  alt="Placeholder image"
+                >
+              </figure>
+            </div>
+            <div class="card-content">
+              <p class="title">Lorem ipsum</p>
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>.
+                <a href="#">#css</a>
+                <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img
+                  src="https://bulma.io/images/placeholders/1280x960.png"
+                  alt="Placeholder image"
+                >
+              </figure>
+            </div>
+            <div class="card-content">
+              <p class="title">Lorem ipsum</p>
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>.
+                <a href="#">#css</a>
+                <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img
+                  src="https://bulma.io/images/placeholders/1280x960.png"
+                  alt="Placeholder image"
+                >
+              </figure>
+            </div>
+            <div class="card-content">
+              <p class="title">Lorem ipsum</p>
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>.
+                <a href="#">#css</a>
+                <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <a href="mailto:info@ti-ce.de">info@ti-ce.de</a>
+        </p>
+
+        <p>
+          <a href="tel:01632865080">0163 2865080</a>
+        </p>
+        <p>
+          <strong>ti-ce.de</strong> made by
+          <a href="https://jgthms.com">Web Bakery</a>
+        </p>
+        <p>
+          Design by
+          <a href="https://jgthms.com">r3p0x</a>
+        </p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style>
+.hero-image {
+  float: left;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img src="@/assets/logo.svg" width="140" height="140">
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="false"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <strong>
+            <a href="mailto:info@ti-ce.de">info@ti-ce.de</a>
+          </strong>
+        </div>
+        <div class="navbar-item">
+          <strong>
+            <a href="tel:01632865080">0163 2865080</a>
+          </strong>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  name: "Navigation",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+.navbar-item img {
+  max-height: 5rem;
+}
+.navbar-item a {
+  color: black;
+}
+</style>
